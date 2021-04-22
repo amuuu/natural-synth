3c6a8db66239070e59109d1b694c9b064df9c621
amuuu <aamouie01@gmail.com>
2021-04-22 17:08:45 -0400

    add signup template

--- server/src/main.py
+++ server/src/main.py
@@ -6,7 +6,3 @@ main = Blueprint('main', __name__)
 @main.route('/')
 def index():
     return render_template('index.html')
-
-@main.route('/profile')
-def profile():
-    return render_template('profile.html')
