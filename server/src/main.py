84d484f81d3d042f308a5552dffe0abae4cb16a9
amuuu <aamouie01@gmail.com>
2021-04-22 17:44:38 -0400

    show user's email on index

--- server/src/main.py
+++ server/src/main.py
@@ -1,8 +1,10 @@
 from flask import Blueprint, render_template
+from flask_login import login_required, current_user
 from . import db
 
 main = Blueprint('main', __name__)
 
 @main.route('/')
+@login_required
 def index():
-    return render_template('index.html')
+    return render_template('index.html', email=current_user.email)
