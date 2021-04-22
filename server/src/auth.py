43363e2410c57522e48cbdd969fea93cab04c771
amuuu <aamouie01@gmail.com>
2021-04-22 17:39:58 -0400

    add login

--- server/src/auth.py
+++ server/src/auth.py
@@ -1,5 +1,8 @@
 from flask import Blueprint, render_template, redirect, url_for, request, flash
 from werkzeug.security import generate_password_hash, check_password_hash
+
+from flask_login import login_user
+
 from .models import User
 from . import db
 
@@ -37,3 +40,21 @@ def signup_post():
     db.session.add(new_user)
     db.session.commit()
     return redirect(url_for('auth.login'))
+
+@auth.route('/login', methods=['POST'])
+def login_post():
+    # login code goes here
+    email = request.form.get('email')
+    password = request.form.get('password')
+    remember = True if request.form.get('remember') else False
+
+    user = User.query.filter_by(email=email).first()
+
+    # check if the user actually exists
+    # take the user-supplied password, hash it, and compare it to the hashed password in the database
+    if not user or not check_password_hash(user.password, password):
+        flash('Please check your login details and try again.')
+        return redirect(url_for('auth.login')) # if the user doesn't exist or password is wrong, reload the page
+
+    login_user(user, remember=remember)
+    return redirect(url_for('main.index'))
