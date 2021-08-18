ea116f76e081c9c2f96df3c5e194de9fbd12cb04
amuuu <aamouie01@gmail.com>
2021-08-18 10:54:26 -0400

    clean the code, add modify commands (active selfs and name change) to server code, as some docs for commands and topics

--- server/src/main.py
+++ server/src/main.py
@@ -1,8 +1,10 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, Response, request
 from flask_login import login_required, current_user
 from . import db
 
 from . import mqtthandler
+
+import json
 
 main = Blueprint('main', __name__)
 
@@ -14,3 +16,30 @@ def index():
 @main.route('/nodes', methods=['GET'])
 def node_divs():
     return render_template('nodediv.html', node_info=mqtthandler.divs)
+
+@main.route('/changename', methods=['POST'])
+def change_name():
+    try:
+        data = json.loads(request.data)
+        new_name = data.get('new_name')
+        old_name = data.get('old_name')
+
+        mqtthandler.peripheral_node_name_update(old_name, new_name)
+    except:
+        return Response("Error in changing name.", status=400)
+
+    return Response("Name changed successfully.", status=200)
+
+@main.route('/changeactiveself', methods=['POST'])
+def change_activeself():
+    try:
+        data = request.json
+        name = data.get('Data').get('name')
+        activeself = data.get('Data').get('activeself')
+        mqtthandler.peripheral_node_activeself_update(name, activeself)
+    
+    except:
+        return Response("Error in changing activeself.", status=400)
+
+    return Response("Activeself changed successfully.", status=200)
+
--- server/src/mqtthandler.py
+++ server/src/mqtthandler.py
@@ -1,76 +1,119 @@
 import json
 import paho.mqtt.client as mqtt
 
+# IMPORTANT IMPORTANT 
+# After their name / activeself change, must send status update to server
+# currently the server sends the command, but its own list doesnt change at all
+
+# after each node recieves changes information (whether it being active self or name change), it must send a node_introduction command to the network
+
 """
+JSON FORMAT:
+
+~~~~~~~~~~~~~~~~~~~~~~~~
 topic arduino_send:
-{
-"name/ip": sth,
-"sensor_type": sth,
-"is_active": true
-"val": sth,
-}
+~~~~~~~~~~~~~~~~~~~~~~~~
+1) value_share command:
+    {
+    "cmd":"value_share",
+    "device_name": sth,
+    "val": sth,
+    }
 
+2) node_introduction command:
+    {
+    "cmd":"node_introduction",
+    "device_name": sth,
+    "sensor_type": sth,
+    "is_active": true
+    "old_name": "-"
+    }
+
+~~~~~~~~~~~~~~~~~~~~~~~~
 topic arduino_change:
-{
-"device_name/ip": sth,
-"is_active": sth
-}
+~~~~~~~~~~~~~~~~~~~~~~~~
+3) change_active_self command:
+    {
+    "cmd": "change_active_self"
+    "device_name": sth,
+    "activeself": sth
+    }
 
+4) change_name command:
+    {
+    "cmd": "change_name",
+    "device_name": device,
+    "new_name": new_name
+    }
 """
 
 topic = ['ns/arduino_send',
-         'ns/arduino_change',
-         'ns/server_introduction']
+         'ns/arduino_change']
 
 names = []
 divs = []
 
 # changing the name of a node and inform that node.
-def peripheral_node_name_update(client, device, new_name):
-    client.publish(topic[1], json.dumps({'cmd':'changename', 'device': device, 'new_name': new_name}), 0)
+def peripheral_node_name_update(device, new_name):
+    client_object.publish(topic[1], json.dumps({'cmd':'change_name', 'device_name': device, 'new_name': new_name}), 0)
 
 
 # changing the status of a node. after a node recieves the command to deactivate, it stops sending data until it recieves activation again                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                         
-def peripheral_node_activeself_update(client, device, activeself): # ip or name
-    client.publish(topic[1], json.dumps({'cmd':'changeactiveself','device': device, 'activeself': activeself}), 0)
-
-# server introducing herself in the beginning
-def server_introduction(client):
-    client.publish(topic[2], json.dumps({'name': 'server'}))
-
+def peripheral_node_activeself_update(device, activeself): # ip or name
+    client_object.publish(topic[1], json.dumps({'cmd':'change_active_self','device_name': device, 'activeself': activeself}), 0)
 
 def on_connect(client, userdata, flags, rc):
+    global client_object
+    client_object = client
+
+    count_topics=0
     for i in topic:
         client.subscribe(i)
-    server_introduction(client)
-    print("Subscribed to topics successfully...")
+        count_topics += 1
+    print("Subscribed to " + str(count_topics) + " topics successfully...")
 
 def on_message(client, userdata, msg):
     
+    data = json.loads(msg.payload.decode())
+    
     if msg.topic == topic[0]:
     
-        data = json.loads(msg.payload.decode()) #data.get('product_id')
-    
-        name = data.get('name')
-        sensor_type = data.get('sensor_type')
-        val = data.get('val')
-        is_active = data.get('is_active')
-        if is_active=="true":
-            is_active = True
-        elif is_active == "false":
-            is_active = False
-            
-        if name not in names:
-            print("Node \"" + name + "\" didn't exist before. Added to list now.")
-            names.extend(name)
-            # ADD the respective div's content area in the page
-            divs.append({"name": name, "sensor_type": sensor_type, "val": val, "is_active": is_active})
-        else:
-            # UPDATE the respective div's content area in the page
-            pass
+        cmd = data.get('cmd')
+        name = data.get('device_name')
         
-    
-    #     # we probably don't even need this because server always sends out this message and others only recieve.
-    #     # if msg.topic == topic[1]:
-    #     #     data = json.loads(msg.payload.decode())
+        if cmd == "node_introduction":
 
+            sensor_type = data.get('sensor_type')
+            is_active = data.get('is_active')
+            if is_active=="true":
+                is_active = True
+            elif is_active == "false":
+                is_active = False
+            
+            # there is a new name we haven't seen before. it's either a new device or a device we know but with a new name
+            if name not in names:
+                
+                old_name = data.get('old_name')
+                if old_name == "-":
+                    print("Node \"" + name + "\" didn't exist before. Added to list now.")
+                    names.extend(name)
+                    divs.append({"name": name, "sensor_type": sensor_type, "is_active": is_active})
+
+                else:
+                    for div in divs:
+                        if div['name'] == old_name:
+                            div['name'] = name
+
+            ## the name is known. And hasn't changed. So the only reason that this cmd must've been sent is the fact that active self has changed
+            else:
+                for div in divs:
+                    if div['name'] == name:
+                        div['is_active'] = data.get('is_active')
+                    
+        
+        if cmd == "value_share":
+            
+            val = data.get('val')
+
+            # show in the div
+            # it can be done easily with ajax but doesn't it must be with socket to work properly and seamlessly
